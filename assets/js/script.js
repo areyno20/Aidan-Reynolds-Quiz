dda735666b970be2c1284c2ccf38af2fe72ae0e9
Aidan <aidanreynolds715@gmail.com>
2022-11-03 16:07:01 -0400

    Addition of questions to JS file

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+//Constants
+
+
+
+//Quiz Question Array
+let questionOne = {
+  question: "Which of the following keywords is used to define a variable in Javascript?",
+  answer: ["let", "var",  "Both A and B", "None of the above"],
+  correctAnswer: 3
+};
+
+let questionTwo = {
+  question: "When an operator’s value is NULL, the type of returned by the unary operator is",
+  answer: ["Object", "Boolean",  "Undefined", "Integer"],
+  correctAnswer: 1 
+};
+
+let questionThree ={
+  question: "How do we write a comment in javascript?",
+  answer: ["**", "//",  "/* */", "-?"],
+  correctAnswer: 2     
+};
+
+let questionFour ={
+  question: "How can a datatype be declared to be a constant type?",
+  answer: ["var", "let", "constant", "const"],
+  correctAnswer: 4    
+};
+
+let questionFive ={
+  question: "Which of the following methods is used to access HTML elements using Javascript?",
+  answer: ["getElementbyId()", "getElementsByClassName()", "Both A and B", "None of the above"],
+  correctAnswer: 3      
+};
+
+function showQuestion(q){
+  let titleDiv = document.getElementById("title")
+  titleDiv.textContent = q.title
+  let alternative = document.querySelector('.alternative')
+  alternative.forEach(function(element, index){
+    element.textContent= q.alternatives[index]});
+}
+
+showQuestion(questionOne);
+
+let button = document.getElementById('next-button')
